6a48bc8faef659a5cb7848189808e75c2aa755af
mackchay <egoregor2068@gmail.com>
2023-02-16 11:12:04 +0700

    lab1 with parallelism.

--- lab1/main.c
+++ lab1/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define ROOT 0
+
 void init(double *A, double *b, double *x, int N) {
     for (size_t i = 0; i < N * N; i++) {
         if (i % (N + 1) == 0) {
@@ -17,26 +19,59 @@ void init(double *A, double *b, double *x, int N) {
 }
 
 
-void vectorSub(double *a, double *b, double *result, int N) {
+void vector_sub(double *a, double *b, double *result, int N) {
     for (size_t i = 0; i < N; i++) {
         result[i] = a[i] - b[i];
     }
 }
 
-void numberMul(double *a, double number, int N) {
+
+void mul_on_scalar(double *a, double number, int N) {
     for (size_t i = 0; i < N; i++) {
         a[i] = number * a[i];
     }
 }
 
-void matrixVectorMul(double *A, double *x, double *result, int N) {
+
+void print_vector(double *x, int N) {
+    printf("x = ( ");
     for (size_t i = 0; i < N; i++) {
+        printf("%.3lf ", x[i]);
+    }
+    printf(")\n");
+}
+
+
+void matrix_vector_mul(double *A, double *x, double *result, int N) {
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    for (size_t i = rank * (N / size); i < rank * N / size + N / size; i++) {
         result[i] = 0;
         for (size_t j = 0; j < N; j++) {
             result[i] += A[i * N + j] * x[j];
         }
     }
+    MPI_Send(result + rank * (N / size), N / size, MPI_DOUBLE, ROOT, 0, MPI_COMM_WORLD);
+    //printf("Process %d send information\n", rank);
+    if (rank == ROOT) {
+        for (int i = 0; i < size; i++) {
+            MPI_Recv(result + i * (N / size), N / size, MPI_DOUBLE, i, 0, MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+            //printf("From process %d received data.\n", i);
+        }
+        for (int i = N - N % size - 1; i < N; i++) {
+            result[i] = 0;
+            for (size_t j = 0; j < N; j++) {
+                result[i] += A[i * N + j] * x[j];
+            }
+        }
+    }
+
+    MPI_Bcast(result, N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 }
+
 
 double scalar(double *a, int N) {
     double result = 0.0;
@@ -46,30 +81,24 @@ double scalar(double *a, int N) {
     return result;
 }
 
-void vectorCalculation(double *A, double *b, double *x, int N) {
+
+void vector_calculation(double *A, double *b, double *x, int N) {
     double t = 0.01;
     double e = 0.00001;
     double criteria = e + 1.0;
     double *Ax = malloc(N * sizeof(double));
     double *num = malloc(N * sizeof(double));
     while (criteria >= e) {
-        matrixVectorMul(A, x, Ax, N);
-        vectorSub(Ax, b, num, N);
+        matrix_vector_mul(A, x, Ax, N);
+        vector_sub(Ax, b, num, N);
         criteria = scalar(num, N) / scalar(b, N);
-        numberMul(num, t, N);
-        vectorSub(x, num, x, N);
+        mul_on_scalar(num, t, N);
+        vector_sub(x, num, x, N);
     }
     free(num);
     free(Ax);
 }
 
-void printVector(double *x, int N) {
-    printf("x = ( ");
-    for (size_t i = 0; i < N; i++) {
-        printf("%.3lf ", x[i]);
-    }
-    printf(")");
-}
 
 int main(int argc, char* argv[]) {
 
@@ -79,16 +108,21 @@ int main(int argc, char* argv[]) {
         return errCode;
     }
 
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int N = 15;
-    double *A = malloc(N * N * sizeof(double *));
+    int N = 10;
+    double *A = malloc(N * N * sizeof(double));
 
     double *b = malloc(N * sizeof(double));
     double *x = malloc(N * sizeof(double));
 
     init(A, b, x, N);
-    vectorCalculation(A, b, x, N);
-    printVector(x, N);
+    vector_calculation(A, b, x, N);
+
+    if (rank == ROOT) {
+        print_vector(x, N);
+    }
 
 
     free(A);
