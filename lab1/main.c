51a368fe2ede4a9b3500d62091137a6a09dbdf9b
mackchay <egoregor2068@gmail.com>
2023-02-19 20:15:22 +0700

    Lab1 remade.

--- lab1/main.c
+++ lab1/main.c
@@ -5,10 +5,10 @@
 
 #define RANK_ROOT 0
 #define TAG 0
-#define EPSILON 0.00000001
-#define TAU 0.0001
+#define EPSILON 0.00001
+#define TAU 0.01
 
-void init(double *A, double *b, double *x, int N) {
+void init(double *A, double *b, int N) {
     for (int i = 0; i < N * N; i++) {
         if (i % (N + 1) == 0) {
             A[i] = 2.0;
@@ -17,7 +17,6 @@ void init(double *A, double *b, double *x, int N) {
         }
     }
     for (int i = 0; i < N; i++) {
-        x[i] = 0;
         b[i] = N + 1;
     }
 }
@@ -47,37 +46,10 @@ void print_vector(double *x, int N) {
 
 
 void matrix_vector_mul(double *A, double *x, double *result, int N) {
-    int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     //Multiplication of rows of matrices corresponding to processes by a vector
-    for (int i = rank * (N / size); i < rank * N / size + N / size; i++) {
-        result[i] = 0;
-        for (int j = 0; j < N; j++) {
-            result[i] += A[i * N + j] * x[j];
+        for (int i = 0; i < N; i++) {
+            result[i] += A[i] * x[i];
         }
-    }
-    if (rank != RANK_ROOT) {
-        MPI_Send(result + rank * (N / size), N / size, MPI_DOUBLE,
-                 RANK_ROOT, TAG, MPI_COMM_WORLD);
-    }
-
-    //Root process accepts values from other processors
-    if (rank == RANK_ROOT) {
-        for (int other_rank = RANK_ROOT + 1; other_rank < size; other_rank++) {
-            MPI_Recv(result + other_rank * (N / size), N / size, MPI_DOUBLE, other_rank, TAG, MPI_COMM_WORLD,
-                     MPI_STATUS_IGNORE);
-        }
-        //Work with remaining unallocated part by processes.
-        for (int i = N - N % size; i < N; i++) {
-            result[i] = 0;
-            for (int j = 0; j < N; j++) {
-                result[i] += A[i * N + j] * x[j];
-            }
-        }
-    }
-    MPI_Bcast(result, N, MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
 }
 
 
@@ -92,36 +64,63 @@ double euclidean_norm(double *a, int N) {
 
 
 void vector_calculation(double *A, double *b, double *x, int N) {
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     double criteria = (EPSILON) + 1.0;
     double *Ax = calloc(N, sizeof(double));
+    double *AxBuf = calloc(N / size, sizeof(double));
+    double *ABuf = calloc(N * (N / size), sizeof(double));
+
+    MPI_Bcast(b, N, MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
+    MPI_Bcast(x, N, MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
+    MPI_Scatter(A, N * (N / size), MPI_DOUBLE, ABuf, N * (N / size),
+                    MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
     while (criteria >= EPSILON) {
-        matrix_vector_mul(A, x, Ax, N);
-        vector_sub(Ax, b, Ax, N);
-        criteria = euclidean_norm(Ax, N) / euclidean_norm(b, N);
-        mul_on_scalar(Ax, TAU, N);
-        vector_sub(x, Ax, x, N);
+        MPI_Scatter(Ax, N / size, MPI_DOUBLE, AxBuf, N / size,
+                    MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
+        matrix_vector_mul(ABuf, x, AxBuf, N * (N / size));
+        vector_sub(AxBuf, b, AxBuf, N / size);
+        MPI_Allgather(AxBuf, N / size, MPI_DOUBLE, Ax, N / size,
+                      MPI_DOUBLE, MPI_COMM_WORLD);
+
+        if (rank == RANK_ROOT) {
+            criteria = euclidean_norm(Ax, N) / euclidean_norm(b, N);
+            mul_on_scalar(Ax, TAU, N);
+            vector_sub(x, Ax, x, N);
+        }
+        MPI_Bcast(x, N, MPI_DOUBLE, RANK_ROOT, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
     }
     free(Ax);
+    free(AxBuf);
+    free(ABuf);
 }
 
 
 int main(int argc, char* argv[]) {
 
-    int errCode, rank, N = 18880;
+    int errCode, rank, N = 100;
     if ((errCode = MPI_Init(&argc, &argv)) != 0)
     {
         return errCode;
     }
     double begin, end;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    double *A = calloc(N * N, sizeof(double));
+    double *A = NULL;
     double *b = calloc(N, sizeof(double));
     double *x = calloc(N, sizeof(double));
 
+    if (rank == RANK_ROOT) {
+        A = calloc(N * N, sizeof (double));
+        init(A, b, N);
+    }
+
     begin = MPI_Wtime();
-    init(A, b, x, N);
     vector_calculation(A, b, x, N);
     end = MPI_Wtime();
+
     if (rank == RANK_ROOT) {
         print_vector(x, N);
         printf("The elapsed time is %lf seconds", end - begin);
